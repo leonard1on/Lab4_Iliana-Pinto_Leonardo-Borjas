477b1b05e4e5ecfdc7abd8b7a80b0cec20f0e1f7
Leonardo Brojas <leonardborj@gmail.com>
2017-05-12 17:20:20 -0600

    Metodos de modificar

--- Lab4_Iliana_Leonardo/src/lab4_iliana_leonardo/Temporal.java
+++ Lab4_Iliana_Leonardo/src/lab4_iliana_leonardo/Temporal.java
@@ -5,6 +5,7 @@
  */
 package lab4_iliana_leonardo;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -12,10 +13,10 @@ import java.util.Scanner;
  * @author iliana
  */
 public class Temporal {
-    Scanner sc = new Scanner (System.in);
-    
-    
-    public void metodo(){
+
+    static Scanner sc = new Scanner(System.in);
+
+    public void metodo() {
         System.out.println("Ingrese el tamaño en metros");
         double tamaño = sc.nextInt();
         System.out.println("Ingrese la altura");
@@ -37,12 +38,100 @@ public class Temporal {
             case 4:
                 System.out.println("");
         }
-    }        
+    }
+
+    public static ArrayList<Producto> modproducto(ArrayList<Producto> productos) {
+        for (Producto p : productos) {
+            System.out.println(productos.indexOf(p) + " " + p);
+        }
+        System.out.println("Cual producto desea modificar?");
+        int espacio = sc.nextInt();
+        if (espacio < productos.size()) {
+            System.out.println("Cual es la fecha de emision?");
+            String fechaemision = sc.next();
+            System.out.println("Cual es la vida util?");
+            int vidautil = sc.nextInt();
+            System.out.println("Cual es el precio?");
+            double precio = sc.nextDouble();
+            System.out.println("Cual es el tamano?");
+            int tamano = sc.nextInt();
+            System.out.println("Cual es la descripcion?");
+            String descripcion = sc.next();
+            System.out.println("Cual es el nombre?");
+            String nombre = sc.next();
+            System.out.println("Numero de producto?");
+            int numero = sc.nextInt();
+
+            productos.get(espacio).setFecha(fechaemision);
+            productos.get(espacio).setVidaUtil(vidautil);
+            productos.get(espacio).setPrecio(precio);
+            productos.get(espacio).setTamaño(tamano);
+            productos.get(espacio).setDescripcion(descripcion);
+            productos.get(espacio).setNombre(nombre);
+            productos.get(espacio).setNumero(numero);
+        }
+        return productos;
+    }
+
+    public static ArrayList<Cliente> modCliente(ArrayList<Cliente> clientes, ArrayList<Producto> productos) {
+        int resp=0;
+        for (Cliente c : clientes) {
+            System.out.println(clientes.indexOf(c) + " " + c);
+        }
+        System.out.println("Ingrese el cliente que quiere modificar?");
+        int espacio = sc.nextInt();
+        if (espacio < clientes.size()) {
+            System.out.println("Cual es el nombre del empleado");
+            String nombre = sc.next();
+            System.out.println("Cual es el ID");
+            String id = sc.next();
+            System.out.println("Cual es la edad");
+            int edad = sc.nextInt();
+            System.out.println("Cual es la altura");
+            double altura = sc.nextDouble();
+            System.out.println("Cual es el peso");
+            double peso = sc.nextDouble();
+            System.out.println("Cual es la residencia");
+            String residencia = sc.next();
+            System.out.println("Cuanto dinero tiene en el bolsillo");
+            double dinero = sc.nextDouble();
+            System.out.println("En que fecha compro su primer producto?");
+            String fecha = sc.next();
             
+            clientes.get(espacio).setNombre(nombre);
+            clientes.get(espacio).setId(id);
+            clientes.get(espacio).setEdad(edad);
+            clientes.get(espacio).setAltura(altura);
+            clientes.get(espacio).setPeso(peso);
+            clientes.get(espacio).setResidencia(residencia);
+            clientes.get(espacio).setDinero(dinero);
+            clientes.get(espacio).setPrimercompra(fecha);
+            do{
+                    for (Producto p : productos) {
+                        System.out.println(productos.indexOf(p)+" "+p);
+                    }
+                        System.out.println("Cual producto ha comprado?");
+                    int espacio2=sc.nextInt();
+                        if (espacio2<productos.size()) {
+                            clientes.get(espacio).getProducto().add(productos.get(espacio2));
+                        }
+                        System.out.println("Desea agregar otro producto? s/n");
+                        resp=sc.next().charAt(0);
+                    }while(resp!='n');
             
+        }
+        return clientes;
+    }
+    
+    public static ArrayList<Empleado> modempleado(ArrayList<Empleado> empleados){
+        for (Empleado e : empleados) {
+            System.out.println(empleados.indexOf(e)+" "+e);
+        }
+        System.out.println("Cual empleado desea modificar?");
+        int espacio=sc.nextInt();
+        if (espacio<empleados.size()) {
             
-            
-            
-            
-            
+        }
+    }
+
 }
